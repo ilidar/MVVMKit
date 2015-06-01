0012217cdb66e61c19f570b70b3eff3948d05d9b
Denys Kotelovych <denys.kotelovych@gmail.com>
2015-06-01 13:13:31 +0300

    Adopted xctool;

--- travis/script.sh
+++ travis/script.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -ev
 
-xcodebuild \
- -workspace MVVMKit.xcworkspace \
- -scheme MVVMKitTests \
- -sdk iphonesimulator \
- -arch i386 \
- build test
+xctool \
+  -workspace MVVMKit.xcworkspace \
+  -scheme MVVMKit \
+  -sdk iphonesimulator \
+  -arch i386 \
+  test
