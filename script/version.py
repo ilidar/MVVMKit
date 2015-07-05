5c40c5b432210d8187f7ea7adde3387ef460fef7
Denys Kotelovych <denys.kotelovych@gmail.com>
2015-07-05 15:09:20 +0300

    Script sugar;

--- script/version.py
+++ script/version.py
@@ -5,25 +5,26 @@ import plistlib
 from subprocess import check_output
 
 
-def set_version(version):
-    return check_output(
-        ["/usr/bin/agvtool new-version -all %s" % version],
-        shell=True)
-
-
-def get_next_version(version):
+def get_version_next(version):
     components = str(version).split('.')
     init, last = components[:-1], components[-1:]
     init.append(str(int(last[0]) + 1))
     return ".".join(init)
 
 
-def set_version_auto():
+def set_version(version):
+    return check_output(
+        ["/usr/bin/agvtool new-version -all %s" % version],
+        shell=True)
+
+
+def set_version_automatically():
     plist = plistlib.readPlist("MVVMKit/Info.plist")
     current_version = plist["CFBundleVersion"]
-    next_version = get_next_version(current_version)
+    next_version = get_version_next(current_version)
     set_version(next_version)
-    print current_version, "~>", next_version
+    print "Updated version from [%s] to [%s]." \
+        % (current_version, next_version)
 
 
 def main():
@@ -34,7 +35,7 @@ def main():
 
     args = parser.parse_args()
     if args.auto:
-        set_version_auto()
+        set_version_automatically()
     elif args.new:
         set_version(args.new)
 
