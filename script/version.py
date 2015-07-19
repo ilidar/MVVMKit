e40437588b80597aee548a965273762f9f14b4a3
Denys Kotelovych <denys.kotelovych@gmail.com>
2015-07-19 12:57:49 +0300

    Added integration with git and cocoapods;

--- script/version.py
+++ script/version.py
@@ -8,6 +8,7 @@ import fileinput
 
 PODSPEC_PATH = "MVVMKit.podspec"
 PLIST_PATH = "MVVMKit/Info.plist"
+COCOAPODS_REPO_PATH = "CocoaPods-Private"
 
 
 def increment_version(version):
@@ -48,10 +49,44 @@ def set_version_automatically():
     set_version(next_version)
 
 
+def git_add_all():
+    subprocess.check_output(["git", "add", "-A", "."])
+
+
+def git_commit(message):
+    subprocess.check_output(["git", "commit", "-m", message])
+
+
+def git_push():
+    subprocess.check_output(["git", "push"])
+    subprocess.check_output(["git", "push", "--tags"])
+
+
+def git_tag(name, message):
+    subprocess.check_output(["git", "tag", "-a", name, "-m", message])
+
+
+def pod_push():
+    args = ["pod", "repo", "push", COCOAPODS_REPO_PATH, PODSPEC_PATH]
+    subprocess.check_output(args)
+
+
+def push_version():
+    version = get_version()
+    git_add_all()
+    git_commit("Bumped version %s;" % version)
+    git_tag(version, "%s tag;" % version)
+    git_push()
+    pod_push()
+
+
 def main():
     parser = argparse.ArgumentParser(description='Version bumper')
     parser.add_argument("-n", "--new", help="set new version number")
     parser.add_argument("-a", "--auto", help="autoincrement version number",
+                        action="store_true")
+    parser.add_argument("-p", "--push",
+                        help="commit and push changes to repositories",
                         action="store_true")
 
     args = parser.parse_args()
@@ -59,6 +94,8 @@ def main():
         set_version_automatically()
     elif args.new:
         set_version(args.new)
+    elif args.push:
+        push_version()
 
 
 if __name__ == "__main__":
