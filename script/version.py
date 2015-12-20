759ebec44014a26eeba0bf812fb4ea338d1b16b2
Denys Kotelovych <denys.kotelovych@gmail.com>
2015-12-20 21:20:54 +0200

    Updated versioning script;

--- script/version.py
+++ script/version.py
@@ -5,11 +5,7 @@ import plistlib
 import subprocess
 import fileinput
 
-
-PODSPEC_PATH = "MVVMKit.podspec"
 PLIST_PATH = "MVVMKit/Info.plist"
-COCOAPODS_REPO_PATH = "CocoaPods-Private"
-
 
 def increment_version(version):
     components = str(version).split('.')
@@ -24,20 +20,12 @@ def get_version():
     return plist["CFBundleVersion"]
 
 
-def set_podspec_version(new_version):
-    global PODSPEC_PATH
-    old_version = get_version()
-    for line in fileinput.input(PODSPEC_PATH, inplace=True):
-        print line.replace(old_version, new_version),
-
-
 def set_plist_version(version):
     subprocess.check_output(["agvtool", "new-version", "-all", version])
 
 
 def set_version(version):
     old_version = get_version()
-    set_podspec_version(version)
     set_plist_version(version)
     print "Updated version from [%s] to [%s]." \
         % (old_version, version)
@@ -66,18 +54,12 @@ def git_tag(name, message):
     subprocess.check_output(["git", "tag", "-a", name, "-m", message])
 
 
-def pod_push():
-    args = ["pod", "repo", "push", COCOAPODS_REPO_PATH, PODSPEC_PATH]
-    subprocess.check_output(args)
-
-
 def push_version():
     version = get_version()
     git_add_all()
     git_commit("Bumped version %s;" % version)
     git_tag(version, "%s tag;" % version)
     git_push()
-    pod_push()
 
 
 def main():
